c7cddce85007b16796cea6e5e34acc75f2f33f3a
Enrique Barragan <ebarragan1997@gmail.com>
2017-09-14 18:12:33 -0500

    update migration

--- db/migrate/028_add_more_changeset_indexes.rb
+++ db/migrate/028_add_more_changeset_indexes.rb
@@ -2,12 +2,18 @@ require "migrate"
 
 class AddMoreChangesetIndexes < ActiveRecord::Migration
   def self.up
+    execute <<-SQL
+      CREATE EXTENSION btree_gist
+    SQL
     add_index "changesets", ["created_at"], :name => "changesets_created_at_idx"
     add_index "changesets", ["closed_at"], :name => "changesets_closed_at_idx"
     add_index "changesets", %w[min_lat max_lat min_lon max_lon], :name => "changesets_bbox_idx", :using => "GIST"
   end
 
   def self.down
+    execute <<-SQL
+      DROP EXTENSION btree_gist
+    SQL
     remove_index "changesets", :name => "changesets_bbox_idx"
     remove_index "changesets", :name => "changesets_closed_at_idx"
     remove_index "changesets", :name => "changesets_created_at_idx"
